fda342ff2067be33395f9b2b98c08f88bad74da6
Cholaria23 <38050622+Cholaria23@users.noreply.github.com>
2018-04-04 15:55:32 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+function evenNum($even) {
+ $arr1=[];
+ 	 for ($i=0;$even!=0&$even>0; $i++){
+ 	 	if ($even>0&$even%2==0){
+ 	 		$even=$arr1[]=$even-2;		
+ 	 	}else{ $even--;
+ 	 		$arr1[]=$even;
+ 	 		echo "No Even! or Negative Number!";}
+ 	}
+return $arr1;
+}
