7be7e978e9565b43acef1a845e5759943de78a1b
Cholaria23 <38050622+Cholaria23@users.noreply.github.com>
2018-04-05 08:45:17 +0300

    Update class_Array_Even.php

--- class_Array_Even.php
+++ class_Array_Even.php
@@ -1,4 +1,6 @@
-function evenNum($even) {
+<?php
+class Array_Even {
+function even_num($even) {
  $arr1=[];
  	 for ($i=0;$even!=0&$even>0; $i++){
  	 	if ($even>0&$even%2==0){
@@ -8,5 +10,8 @@ function evenNum($even) {
  	 		echo "No Even! or Negative Number!";}
  	}
 return $arr1;
+ }
 }
-var_dump(evenNum(10));
+
+$obj= new Array_Even();
+var_dump(obj->even_num(10));
