d3097d6919eccb6285e225205dd092443e60ec74
unknown <C:\Users\technolog\AppData\Roaming\The Bat! Pwd>
2018-04-06 14:16:36 +0300

    README

---
+++ class_Array_Summa.php
@@ -0,0 +1,40 @@
+﻿<?php
+// *******************************************************
+// *Задача №5 найти сумму  массива 
+// 
+class Array_Summa
+{ public $arr;
+	function __construct (array $arr)
+	{$this->arr =$arr;}
+		public function myCount()
+		{
+			$c =0;
+			foreach ($this->arr as $value) 
+			{
+				$c++;# code...
+			}
+			return $c;		
+		}	
+			public function summa_arr()
+				{   $summa= 0;
+				 	$count = $this->myCount($this->arr);
+					for ($i=0; $i!=$count;$i++) 
+					{   
+						$max = $this->arr[$i];
+						$result =$this->arr[++$i];
+						// echo "Первое число в масиве =",$max,'<p></p>';
+						// echo "Second number =", $result,'<p></p>';
+						$summa = $summa + $max + $result;
+					    // echo "Сумма=", $summa,'<p></p>';
+			
+					}
+				 
+				 return $summa;
+				}
+					
+}
+
+$arr=[5,8,9,10,20,52,56,5];
+$obj= new Array_Summa($arr);
+var_dump($obj->arr);
+echo "Сумма элементов массива =",($obj->summa_arr());
