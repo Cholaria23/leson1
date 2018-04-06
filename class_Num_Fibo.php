b25da6babbf5b04acb58eeffa5bb5c007496c404
unknown <C:\Users\technolog\AppData\Roaming\The Bat! Pwd>
2018-04-06 11:29:05 +0300

    class_Num_Fibo

--- class_Num_Fibo.php
+++ class_Num_Fibo.php
@@ -1,9 +1,16 @@
-function fiboNum($count){
-    $arr =[0,1]; 
-    for ($i=1;$i<$count;$i++){
-        $arr[] = $arr[$i]+$arr[$i-1];
-    }
-    return $arr;
+/*
+* Задача №2) Выводит определенное количество элементов  последовательности Фибоначчи.
+*/
+class Numbers_Fibonachi
+{
+	function fiboNum($count){
+	    $arr =[0,1]; 
+	    for ($i=1;$i<$count;$i++){
+	        $arr[] = $arr[$i]+$arr[$i-1];
+	    }
+	    return $arr;
+	}
 }
 
-var_dump(fiboNum(10));
+$obj= new Numbers_Fibonachi();
+var_dump($obj->fiboNum(10));
