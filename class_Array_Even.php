5ae1142f32a41954c7c655e816876059911e87c5
cholaria23 <cholaria23@gmail.com>
2018-04-14 08:34:06 +0300

    class_Array_Even.php

--- class_Array_Even.php
+++ class_Array_Even.php
@@ -3,15 +3,21 @@
 * Задача №1 Вывод всех положительных четные чисел, которые меньше заданного
 */
 class Array_Even 
- {function even_num($even) 
-  {$arr1=[];
+ {
+ 	function even_num($even) 
+  {
+  	$arr1=[];
  	for ($i=0;$even!=0&$even>0; $i++)
-      {if ($even>0&$even%2==0)
-        {$even=$arr1[]=$even-2;		
- 	 	 }else{ $even--;// Блок else для преобразования нечетного числа в четное
+      {
+      	if ($even>0&$even%2==0)
+        {
+          $even=$arr1[]=$even-2;		
+ 	 	 }
+ 	 	  else{ 
+ 	 	 		$even--;// Блок else для преобразования нечетного числа в четное
  	 		    $arr1[]=$even;
  	 		    echo "No Even! or Negative Number!";
-          }
+          	   }
  	  }
  return $arr1;
  }
