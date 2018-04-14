4e380487db7c0e1ae81c21a6a6d01a72259e0052
cholaria23 <cholaria23@gmail.com>
2018-04-14 11:49:53 +0300

    class_Array_In

---
+++ class_Array_In.php
@@ -0,0 +1,21 @@
+﻿<?php
+  /*
+  * Задача № 7 Реализация функции нахождения заданого числа или строки в массиве
+  */
+  // array( ... non-duplicate string and integer values ... )
+  // $needle = 32;
+  // $arr = array(32,34,5,6,8,9);
+  $needle= 'Max';
+  $arr = array('Max',"Min","Big","Small","tiny");
+ 
+function in_array_i($needle, array $arr, $i = 0)
+{
+  $c = count($arr);
+
+  for (;$i < $c; ++$i)
+    if ($arr[$i] == $needle) return true; 
+  return false;   
+}
+in_array_i($needle,$arr);
+var_dump (in_array_i($needle,$arr));
+
