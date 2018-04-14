f9837889c5dd267b4b2d338135c10bfaf81867f2
cholaria23 <cholaria23@gmail.com>
2018-04-14 10:28:14 +0300

    class_Array_Summa.php

--- class_Array_Summa.php
+++ class_Array_Summa.php
@@ -3,38 +3,39 @@
 // *Задача №5 найти сумму  массива 
 // 
 class Array_Summa
-{ public $arr;
+{ 
+	public $arr;
+	
 	function __construct (array $arr)
-	{$this->arr =$arr;}
-		public function myCount()
+	{
+		$this->arr =$arr;
+	}
+		public function myCount (array $arr)
 		{
-			$c =0;
-			foreach ($this->arr as $value) 
+		$c =0;
+			foreach ($arr as $value) 
 			{
 				$c++;# code...
 			}
 			return $c;		
 		}	
-			public function summa_arr()
-				{   $summa= 0;
-				 	$count = $this->myCount($this->arr);
-					for ($i=0; $i!=$count;$i++) 
+		public function summa_arr(array $arr)
+			{   $summa= 0;
+				$count = $this->myCount($arr);
+
+					for ($i=0; $i<$count; $i++) 
 					{   
-						$max = $this->arr[$i];
-						$result =$this->arr[++$i];
-						// echo "Первое число в масиве =",$max,'<p></p>';
-						// echo "Second number =", $result,'<p></p>';
-						$summa = $summa + $max + $result;
+						$summa = $summa + $arr[$i];
 					    // echo "Сумма=", $summa,'<p></p>';
 			
 					}
 				 
-				 return $summa;
+				 return $summa; $arr;
 				}
 					
 }
 
 $arr=[5,8,9,10,20,52,56,5];
 $obj= new Array_Summa($arr);
-var_dump($obj->arr);
-echo "Сумма элементов массива =",($obj->summa_arr());
+var_dump($obj->summa_arr($arr));
+echo "Сумма элементов массива =",($obj->summa_arr($arr));
