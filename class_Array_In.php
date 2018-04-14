c4a019a12ed0cf7ff6b60fbf0696643131e74734
cholaria23 <cholaria23@gmail.com>
2018-04-14 12:04:32 +0300

    class_Array_In

--- class_Array_In.php
+++ class_Array_In.php
@@ -3,19 +3,39 @@
   * Задача № 7 Реализация функции нахождения заданого числа или строки в массиве
   */
   // array( ... non-duplicate string and integer values ... )
-  // $needle = 32;
-  // $arr = array(32,34,5,6,8,9);
-  $needle= 'Max';
-  $arr = array('Max',"Min","Big","Small","tiny");
  
-function in_array_i($needle, array $arr, $i = 0)
-{
-  $c = count($arr);
+class Array_In 
+{ 
+	public $arr;
 
-  for (;$i < $c; ++$i)
-    if ($arr[$i] == $needle) return true; 
-  return false;   
+	function __construct(array $arr)
+	{
+		$this->arr = $arr;
+	}
+
+	public function myCount(array $arr)
+	{
+		$c=0;
+		foreach ($arr as $value) 
+		{
+		 $c++;	# code...
+		}
+	return $c;
+	}
+	public function in_array_i($needle, array $arr, $i = 0)
+	{
+	  $c = $this->myCount($arr);
+
+	  for (;$i < $c; ++$i)
+	    if ($arr[$i] == $needle) return true; 
+	  return false;   
+	}
 }
-in_array_i($needle,$arr);
-var_dump (in_array_i($needle,$arr));
+
+// $needle = 32;
+// $arr = array(32,34,5,6,8,9);
+$needle= 'Max';
+$arr = array('Max',"Min","Big","Small","tiny");
+$obj= new Array_In($arr);
+var_dump ($obj->in_array_i($needle,$arr));
 
