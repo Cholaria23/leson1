a383c96c394ca59cb5611a3f2e02abedfd63a964
cholaria23 <cholaria23@gmail.com>
2018-05-17 12:15:19 +0300

    Создание шаблона article с последующим его выводом

--- my-blog/src/Controller/MainController.php
+++ my-blog/src/Controller/MainController.php
@@ -16,4 +16,15 @@ class MainController extends Controller
         $posts = $this->getDoctrine()->getRepository(Post::class)->findAll();
         return $this->render('main/index.html.twig', compact('posts'));
     }
+
+    /**
+     * @Route("/article/{articleId}", name="article")
+     */
+    public function article($articleId)
+    {
+        $post = $this->getDoctrine()->getRepository(Post::class)->find($articleId);
+        return $this->render('main/article.html.twig', [
+            'post' => $post,
+        ]);
+    }
 }
