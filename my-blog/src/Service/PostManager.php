150460ca5426394b9ce36e73bc1a0f79d684c97e
cholaria23 <cholaria23@gmail.com>
2018-06-14 14:57:36 +0300

    add comment

--- my-blog/src/Service/PostManager.php
+++ my-blog/src/Service/PostManager.php
@@ -3,23 +3,34 @@
 namespace App\Service;
 
 use App\Repository\TagRepository;
+use App\Entity\Comments;
+use App\Repository\CommentsRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\FormFactoryInterface;
 
 class  PostManager
 {
 
-	private $repository;
+	private $tagRepository;
+	private $commentRepository;
+	private $formFactory;
+	private $entityManager;
 
-	public function __construct(TagRepository $repository)
+	public function __construct(EntityManagerInterface $entityManager,FormFactoryInterface $formFactory,CommentsRepository $commentRepository,TagRepository $tagRepository)
 	{
-		$this->repository = $repository;
+		$this->tagRepository = $tagRepository;
+		$this->formFactory = $formFactory;
+		$this ->entityManager = $entityManager;
+		$this ->commentRepository = $commentRepository;
+
 	}
 
-	public function toDo()
-	{
+	// public function toDo()
+	// {
 		
-		$tags = $this->getDoctrine()->getRepository(Tag::class)->findAll();
-		return true;
-	}
+	// 	$tags = $this->getDoctrine()->getRepository(Tag::class)->findAll();
+	// 	return true;
+	// }
 
 // *
 //  * 
@@ -30,21 +41,32 @@ class  PostManager
 //  * 
  
 
-public function saveComment():Response
+// public function saveComment():Response
+public function saveComment()
  {
-     $entityManager = $this->getDoctrine()->getManager();
+     // dump($this->tagRepository);
+     // dump ($this->entityManager);
+     // dump($this->formFactory);
+     // dump($this->commentRepository);
+     // die;
 
      $comment = new Comments();
- 	 $comment->setComment($_POST['comments']);
-     $comment -> setComment($_POST['comment']);
-     $comment-> setEmail($_POST['email']);
-     $comment-> setAuthor($_POST['author']);
-     $comment-> setUrl($_POST['url']);
+     // $form = $this->createForm(CommentsType::class, $comment);
+     $form ->handleRequest($request);
+     if ($form->isSubmitted() && $form->isValid())
+     {
+     	$entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($comment);
+	    $entityManager->flush();
+
+     }
+ 	 // $comment->setComment($_POST['comments']);
+   //   $comment -> setComment($_POST['comment']);
+   //   $comment-> setEmail($_POST['email']);
+   //   $comment-> setAuthor($_POST['author']);
+   //   $comment-> setUrl($_POST['url']);
    
-
-     $entityManager->persist($comment);
-
-     $entityManager->flush();
+     
      return new Response('Save coment'.$comment->getId());
  }
 }
