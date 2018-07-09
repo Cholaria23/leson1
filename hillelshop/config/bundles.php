1e16058aabd28e1bb3c50b9c05358a6f3cab23c4
cholaria23 <cholaria23@gmail.com>
2018-07-09 10:10:21 +0300

    Add Sonata Admin Panel

--- hillelshop/config/bundles.php
+++ hillelshop/config/bundles.php
@@ -17,5 +17,8 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Sonata\DatagridBundle\SonataDatagridBundle::class => ['all' => true],
     Sonata\BlockBundle\SonataBlockBundle::class => ['all' => true],
+    Sonata\CoreBundle\SonataCoreBundle::class => ['all' => true],
+    Sonata\AdminBundle\SonataAdminBundle::class => ['all' => true],
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
+    Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle::class => ['all' => true],
 ];
